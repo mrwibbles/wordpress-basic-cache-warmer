5394fc8950dda67e3f3451ecf06e962075d0f6fe
Alex Glover <wibbledev@gmail.com>
2024-04-08 14:37:47 +0100

    added files

---
+++ mbd-cache-warmer.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Plugin Name: Cache Warmer Plugin
+ * Plugin URI:        https://mightybigdata.com/
+ * Description:       Adds a cache warmer that runs every X amount of time and warms pages based on the sitemap
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      8.1
+ * Author:            Alexander Glover
+ * Author URI:        https://mightybigdata.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       mbd-cache-warmer-plugin
+ * Domain Path:       /languages
+ */
+require __DIR__ . '/lib/MbdCacheWarmerSetup.php';
+
+$cacheWarmer = new MbdCacheWarmerSetup();
+$cacheWarmer->install();
+
+register_deactivation_hook( __FILE__, 'deactivateMbdCacheWarmerCron');
+
+function deactivateMbdCacheWarmerCron(): void
+{
+    require __DIR__ . '/lib/MbdCacheWarmerHelper.php';
+
+    $helper = new MbdCacheWarmerHelper();
+
+    $helper->writeToLog('deactivate worked');
+    $timestamp = wp_next_scheduled('mbd_cache_warmer');
+    wp_unschedule_event( $timestamp,'mbd_cache_warmer');
+    wp_clear_scheduled_hook('mbd_cache_warmer');
+}
